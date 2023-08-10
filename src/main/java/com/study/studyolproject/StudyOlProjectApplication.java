4de7c15a0eaf6c36b91d2b4b7586784e6d785e44
chen-2426 <574311203@qq.com>
2023-08-10 16:52:12 +0800

    start

---
+++ src/main/java/com/study/studyolproject/StudyOlProjectApplication.java
@@ -0,0 +1,13 @@
+package com.study.studyolproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyOlProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyOlProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/studyolproject/StudyOlProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.studyolproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyOlProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
